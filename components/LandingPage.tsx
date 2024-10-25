3f7e007ead1c309c48dfae3b6a6671efdf71e417
Joe <aidai524@gmail.com>
2024-10-25 18:06:52 +0800

    update footer

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from './Hero';
 import Features from './Features';
+import Footer from './Footer';
 
 const LandingPage: React.FC = () => {
   return (
     <div>
       <Hero />
       <Features />
+      <Footer />
     </div>
   );
 };
